f48c963703add86f7128f4d310383db6bdca39f2
KapeXCIZ <alessio.capecchi.18@gmail.com>
2026-02-08 19:24:57 +0100

    Fixed critical next.js vulnerability and changed about image

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,16 +8,16 @@ export default function AboutSection() {
     return (
         <>
             <Section className={"overflow-visible"} sectionName={"About me"} >
-                <SmallerContainer className={" h-max md:grid md:grid-cols-2 flex flex-col gap-6  md:gap-10 my-10 overflow-visible  content-center items-center"}>
+                <SmallerContainer className={" h-max md:grid md:grid-cols-2 lg:grid-cols-3 flex flex-col gap-6  md:gap-10 my-10 overflow-visible  content-center items-center"}>
                     <Reveal className={"relative overflow-visible "}>
                         <img
-                            src={`${process.env.PUBLIC_URL}/images/ImmagineProfiloOttimizzataNoBg.png`}
+                            src={`${process.env.PUBLIC_URL}/images/1-opt.png`}
                             alt="Alessio Capecchi - Fotografo professionista in Toscana"
-                            className=" rounded-lg max-h-[400px] sm:min-h-[600px] sm:max-h-full scale-100 sm:scale-100 object-cover md:saturate-0 hover:saturate-100 ease-in-out transition duration-300 flex justify-center items-center hover:mix-blend-normal md:mix-blend-hard-light"
+                            className=" rounded-lg max-h-[400px] sm:min-h-[600px] sm:max-h-[10rem] scale-100 sm:scale-100 object-cover md:saturate-0 hover:saturate-100 ease-in-out transition duration-300 flex justify-center items-center hover:mix-blend-normal md:mix-blend-hard-light"
                         />
                         <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-2/3 bg-red-500/30 -z-10 md:size-96 size-64  rounded-full blur-2xl overflow-visible" />
                     </Reveal>
-                    <span className=" h-full flex flex-col py-6 justify-between text-xl gap-4">
+                    <span className=" h-full flex flex-col py-6 justify-between lg:col-span-2 text-xl gap-4">
                         <Reveal>
                             <h1 className="text-secondary md:text-primary serif leading-[90%]">Alessio<br />Capecchi</h1>
                         </Reveal>
